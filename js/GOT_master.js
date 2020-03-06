2034b979fc175ec9e0e4292c707e53a13a1f9963
Shah <s_shah107384@ats-hr420-dm23.fc.ca>
2020-03-06 13:48:43 -0500

    completed with dynamic text

--- js/GOT_master.js
+++ js/GOT_master.js
@@ -3,7 +3,28 @@
   let sigilButtons = document.querySelectorAll(".sigilContainer"),
       lightBox = document.querySelector(".lightbox"),
       houseVideo = lightBox.querySelector("video"),
-      closeButton = lightBox.querySelector(".close-button");
+      closeButton = lightBox.querySelector(".close-button"),
+      currentHouseName = document.querySelector("h1");
+      houseDescription = document.querySelector(".house-info");
+
+
+
+
+      const houseData = [
+        ["Stark", `House Stark of Winterfell is a Great House of Westeros, ruling over the vast region known as the North from their seat in Winterfell. It is one of the oldest lines of Westerosi nobility by far, claiming a line of descent stretching back over eight thousand years. Before the Targaryen conquest, as well as during the War of the Five Kings and Daenerys Targaryen's invasion of Westeros, the leaders of House Stark ruled over the region as the Kings in the North.`],
+        ["Baratheon", `House Baratheon of Storm's End is a legally extinct Great House of Westeros. A cadet branch was formerly the royal house, but House Lannister now controls the throne. House Baratheon traditionally ruled the Stormlands on the eastern coast of Westeros, aptly named for its frequent storms, from their seat of Storm's End.`],
+
+
+        ["Lannister", `House Lannister of Casterly Rock is one of the Great Houses of Westeros, one of its richest and most powerful families and oldest dynasties. It is also the current royal house of the Seven Kingdoms following the extinction of House Baratheon of King's Landing, which had been their puppet house anyway.
+
+        The Lannisters rule over the Westerlands. Their seat is Casterly Rock, a massive rocky promontory overlooking the Sunset Sea which has had habitations and fortifications built into it over the millennia. They are the Lords Paramount of the Westerlands and Wardens of the West. As the new royal house, they also rule directly over the Crownlands from their seat of the Red Keep in King's Landing, the traditional seat of the royal family.`],
+
+
+        ["Greyjoy", `House Greyjoy of Pyke is one of the Great Houses of Westeros. It rules over the Iron Islands, a harsh and bleak collection of islands off the west coast of Westeros, from the castle at Pyke. The head of the house is the Lord Reaper of Pyke.
+
+        House Greyjoy's sigil is traditionally a golden kraken on a black field. Their house words are "We Do Not Sow," although the phrase "What Is Dead May Never Die" is also closely associated with House Greyjoy and their bannermen, as they are associated with the faith of the Drowned God. `]
+
+      ];
 
 
 
@@ -16,6 +37,11 @@
     //capatalize the first letter of the house name with javascript
     // and then add the rest of the house name to it
     let newSource = houseName.charAt(0).toUpperCase() + houseName.slice(1);
+
+
+    // use this varaible to populate the h1 Element on the page
+    currentHouseName.textContent = `House  ${houseData[this.dataset.offset][0]}`;
+    houseDescription.textContent = `${houseData[this.dataset.offset][1]}`;
 
     let targetSource = `video/House-${houseName}.mp4`;
     // debugger;
